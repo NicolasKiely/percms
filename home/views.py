6a653fe796cd21fd06b03a126609ab94f2f42204
nmkiely <nmkiely@ncsu.edu>
2016-08-27 17:16:17 -0400

    Add basic homepage

--- home/views.py
+++ home/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from common import core
-from login import forms
+#from login import forms
 
-# Index page form fields
-index_forms = {
-    'signup_form': forms.signup,
-    'login_form': forms.login
+home_panels = {
+    'title': 'About',
+    'components': [
+        {
+            'type' : 'text',
+            'model': 'raw',
+            'src'  : 'Welcome to my portfolio website'
+        }
+    ]
 }
 
 def index(request):
-    return core.render(request, 'home/index.html', **index_forms)
+    context = {
+        'panels': [ home_panels ]
+    }
+    return core.render(request, 'common/docpage.html', **context)
