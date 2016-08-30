48b79d5a1473c2c61236464b27b6178043b942d3
nmkiely <nmkiely@ncsu.edu>
2016-08-30 00:48:24 -0400

    Initial admin controls for docpages

--- docpage/admin.py
+++ docpage/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import DocPage
+from .models import DocPage, Panel, Component
+
+
+class ComponentsInline(admin.StackedInline):
+    model = Component
+    extra = 1
+
+class PanelsInline(admin.StackedInline):
+    model = Panel
+    extra = 1
+    inlines = [ComponentsInline]
+
+class DocPageAdmin(admin.ModelAdmin):
+    ''' Editor model for doc pages '''
+    fieldsets = [
+        (None, {'fields': ['title', 'category'] }),
+        ('Dates', {'fields': ['dt_published', 'dt_editted']} )
+    ]
+    inlines = [PanelsInline]
 
 # Register your models here.
-admin.site.register(DocPage)
+admin.site.register(Component)
+admin.site.register(Panel)
+admin.site.register(DocPage, DocPageAdmin)
