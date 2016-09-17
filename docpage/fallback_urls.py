4fda492321250d77b55fa1d1ac94b16ec1e097f7
nmkiely <nmkiely@ncsu.edu>
2016-09-17 16:54:42 -0400

    Allow fallbacks to ignore .html suffix

--- docpage/fallback_urls.py
+++ docpage/fallback_urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<category>\w+)/(?P<title>\w+)(.html)', views.view_by_name)
+    url(r'^(?P<category>\w+)/(?P<title>\w+)(.html)?', views.view_by_name)
 ]
