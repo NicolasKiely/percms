c59dcaeb4b46833adba019e1963239586749c2b6
nmkiely <nmkiely@ncsu.edu>
2016-09-25 02:05:22 -0400

    Add project view stubs

--- proj/urls.py
+++ proj/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'editor/$', views.editor_list, name='editor_list'),
-    url(r'editor/(?P<pk>\d+)/\w*$', views.editor_proj, name='editor'),
-    url(r'edit/proj/$', views.edit_proj, name='edit'),
-    url(r'add/$', views.add_proj, name='add'),
+    url(r'editor/(?P<pk>\d+)/\w*$', views.editor, name='editor'),
+    url(r'edit/proj/$', views.edit, name='edit'),
+    url(r'add/$', views.add, name='add'),
     url(r'view/(?P<pk>\d+)/\w*$', views.view, name='view')
 ]
--- proj/views.py
+++ proj/views.py
@@ -8,3 +8,19 @@ def editor_list(request):
         'projects': Proj.objects.order_by('-dt_published')
     }
     return core.render(request, 'proj/editor_list.html', **context)
+
+
+def editor(request, pk):
+    ''' Editor for a project '''
+
+
+def edit(request):
+    ''' Post handle for editting a project '''
+
+
+def add(request):
+    ''' Post handle for adding a new project '''
+
+
+def view(request, pk):
+    ''' Displays project '''
