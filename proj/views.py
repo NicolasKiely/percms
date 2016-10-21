94e56b1739437a8f7c0d7c67e05c3d6845d0e33b
nmkiely <nmkiely@ncsu.edu>
2016-10-21 09:31:17 -0400

    Project editor

--- proj/views.py
+++ proj/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from common import core
 from .models import Proj
 
 
+@login_required
 def editor_list(request):
     ''' Top-levbel editor page for projects '''
     context = {
@@ -11,16 +16,35 @@ def editor_list(request):
     return core.render(request, 'proj/editor_list.html', **context)
 
 
+@login_required
 def editor(request, pk):
     ''' Editor for a project '''
+    project = get_object_or_404(Proj, pk=pk)
+    context = { 'project': project }
+    return core.render(request, 'proj/editor.html', **context)
 
 
+@login_required
 def edit(request):
     ''' Post handle for editting a project '''
 
 
+@login_required
 def add(request):
     ''' Post handle for adding a new project '''
+    projCat = request.POST['category']
+    projTitle = request.POST['title']
+    now = timezone.now()
+
+    # Create new project
+    project = Proj(
+        title=projTitle, category=projCat, dt_published=now
+    )
+    project.save()
+
+    return HttpResponseRedirect(
+        reverse('project:editor', args=(project.id,))
+    )
 
 
 def view(request, pk):
