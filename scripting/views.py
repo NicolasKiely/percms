fc5eca1ad1257814d6e6a6e0f3484187e50bffb0
nmkiely <nmkiely@ncsu.edu>
2017-04-18 21:21:50 -0400

    Link to scripting dashboard

--- scripting/views.py
+++ scripting/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from common import core
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    context = {
+        'title': 'Script Dashboard',
+        'app': {
+            'name': 'Script'
+        },
+
+        'panels': [
+            {
+                'title': 'Scripts',
+                'table': {
+                    'headers': ['Script Name', 'URL'],
+                    'rows': [
+                    ]
+                }
+            }
+        ]
+    }
+    return core.render(request, 'common/app_dashboard.html', **context)
