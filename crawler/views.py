418cbac4d21a5bc1ff68b7bb760d48e878406080
nmkiely <nmkiely@ncsu.edu>
2017-04-18 21:54:26 -0400

    Migrate crawler app dashboard to use common template

--- crawler/views.py
+++ crawler/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from common import core
 from .models import Website
 
@@ -8,9 +9,28 @@ def dashboard(request):
     ''' Admin dashboard for crawler '''
     context = {
         'title': 'Crawler Dashboard',
+        'app': 'Crawler',
+
+        'panels': [
+            {
+                'title': 'Crawler Status',
+                'text': 'Not implemented yet'
+            },
+            {
+                'title': 'Domain Management',
+                'link': reverse('crawler:domain_dashboard'),
+                'table': {
+                    'headers': ['Domain', 'Module', 'Login Profile'],
+                    'rows': [
+                        (w.domain, w.scraper, w.profile.name)
+                        for w in Website.objects.filter(can_crawl=True)
+                    ]
+                }
+            }
+        ],
         'websites': Website.objects.filter(can_crawl=True)
     }
-    return core.render(request, 'crawler/dashboard.html', **context)
+    return core.render(request, 'common/app_dashboard.html', **context)
 
 
 @login_required
