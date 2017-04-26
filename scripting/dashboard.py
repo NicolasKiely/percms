07b77e8f8365068a8e804b11d88af9c9c9766ad6
nmkiely <nmkiely@ncsu.edu>
2017-04-26 17:22:03 -0400

    Remove redundant url column on script listings

--- scripting/dashboard.py
+++ scripting/dashboard.py
@@ -13,7 +13,7 @@ Dashboard.namespace = 'script'
 Script_Dashboard = Model_Dashboard(Dashboard, Script)
 Script_Dashboard.name = 'Script'
 Script_Dashboard.namespace = 'script'
-Script_Dashboard.listing_headers = ['Category', 'Name', 'URL']
+Script_Dashboard.listing_headers = ['Category', 'Name']
 Script_Dashboard.get_listing_record = \
     lambda x: [x.category, x.name]
 
