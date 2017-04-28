15a05f11dc0e83d641ea9dcb701fc3fbbe6c0c75
nmkiely <nmkiely@ncsu.edu>
2017-04-28 18:25:03 -0400

    Check for available model instance in crawler

--- crawler.py
+++ crawler.py
@@ -1,8 +1,25 @@
+'''
+    Usage:
+        python crawler.py
+
+    Will find an inactive crawler instance model, and activate it
+'''
+
 import django
 import os
 import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'percms.settings'
 django.setup()
+
+import crawler.models
+
+
+# Get a crawler 
+try:
+    crawler_instance = crawler.models.Crawler.objects.get(status='inactive')
+except crawler.models.Crawler.DoesNotExist:
+    print 'No instances to run'
+    sys.exit(0)
 
 print 'done'
