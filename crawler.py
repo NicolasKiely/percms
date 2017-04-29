cbba7b3ddb535008cdb2530f9c7303caf042efcc
nmkiely <nmkiely@ncsu.edu>
2017-04-29 13:43:33 -0400

    Add fixed header to crawler

--- crawler.py
+++ crawler.py
@@ -14,6 +14,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'percms.settings'
 django.setup()
 
 import crawler.models
+from percms import safesettings
 
 
 # Get a crawler 
@@ -45,9 +46,33 @@ if site is None:
     sys.exit(0)
 
 # Get webpage to crawl
-page = '/'
-print 'Processing "%s"' % (site.domain + page)
+path = '/'
+webpage = site.domain + path
+print 'Processing "%s"' % webpage
 
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Accept-Encoding': 'identity',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Referer': webpage,
+    'User-Agent': ' '.join([
+        'Mozilla/5.0 (X11; Linux x86_64)',
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Ubuntu Chromium/56.0.2924.76',
+        'Chrome/56.0.2924.76 Safari/537.36'
+    ]),
+}
+request = urllib2.Request(webpage, None, headers)
+response= urllib2.urlopen(request)
+html = response.read()
 
+#print html
+
+local_path = safesettings.UPLOAD_CRAWLER_PATH
+fh = open(local_path + 'data.html', 'w')
+fh.write(html)
+fh.close()
 
 print 'done'
