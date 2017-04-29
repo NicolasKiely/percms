fd7024a4db2c2642e36fd83057f3c2e79d7a8a39
nmkiely <nmkiely@ncsu.edu>
2017-04-28 22:17:22 -0400

    Load crawler config and state in crawler script

--- crawler.py
+++ crawler.py
@@ -22,4 +22,20 @@ except crawler.models.Crawler.DoesNotExist:
     print 'No instances to run'
     sys.exit(0)
 
+# Get configuration
+crawler_config = crawler_instance.config
+if crawler_config is None:
+    print 'Crawler instance is not assigned a configuration'
+    sys.exit(0)
+
+# Get active state
+crawler_state = crawler_instance.active_state
+if crawler_state is None:
+    crawler_state = crawler_config.initial_state
+    if crawler_state is None:
+        print 'Configuration "%s" has no preset state' % crawler_config.name
+        sys.exit(0)
+
+print 'Using crawler configuration "%s:%s"' % (crawler_config.name, crawler_state.name)
+
 print 'done'
