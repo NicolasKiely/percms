c14f8c364c09eb7e8897d8484020dc3a885ea133
nmkiely <nmkiely@ncsu.edu>
2017-08-30 16:45:34 -0400

    Add ecom function for downloading and saving images

--- ecom/runtime.py
+++ ecom/runtime.py
@@ -1,5 +1,9 @@
+from django.utils import timezone
+
 from . import models
 from crawler.models import Webpage
+from filemanager.models import Meta_File
+import urllib2
 
 
 def price_to_cents(pstr):
@@ -7,6 +11,7 @@ def price_to_cents(pstr):
     price_string = pstr.lstrip('$').replace(',', '')
     dollar, cents = price_string.split('.')
     return int(dollar) * 100 + int(cents)
+
 
 def add_product(supplier_name, prod_data):
     ''' Helper function for importing product data from scripts '''
@@ -21,3 +26,27 @@ def add_product(supplier_name, prod_data):
     if 'webpage.id' in prod_data:
         prod.webpage = Webpage.objects.get(pk=prod_data['webpage.id'])
     prod.save()
+
+
+def save_image(supplier_name, image_url):
+    ''' Saves image and returns object '''
+    # Create metadata entry
+    meta_file = Metal_File(
+        name=image_URL,
+        category=supplier_name.replace(' ', '-') + '-image',
+        dt_uploaded = timezone.now(),
+        is_img=True
+    )
+    meta_file.save()
+    sid = str(meta_file.id)
+    save_path = safesettings.UPLOAD_IMAGE_PATH + sid
+
+    # Download image
+    request = urllib2.Request(image_url)
+    image_source = urllib2.urlopen(request)
+
+    # Save to file
+    with open(save_path, 'wb+') as dest:
+        for chunk in image_source.chunks():
+            dest.write(chunk)
+
