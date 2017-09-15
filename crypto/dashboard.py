3941f09238083f5855e0bda3299a966b633a30a8
nmkiely <nmkiely@ncsu.edu>
2017-09-14 23:02:26 -0400

    Add initial crypto app

---
+++ crypto/dashboard.py
@@ -0,0 +1,8 @@
+from common import dashboard as dbd
+from . import models
+from . import views
+
+# App Dashboard
+App_Dashboard = dbd.App_Dashboard()
+App_Dashboard.name = 'Crypto Trading'
+App_Dashboard.namespace = 'crypto'
---
+++ poloniex_scraper.py
@@ -0,0 +1,3 @@
+import crypto.poloniex_api
+
+polo = crypto.poloniex_api.poloniex()
