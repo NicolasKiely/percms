2341e012f356001b8464ba581632669e4d647214
nmkiely <nmkiely@ncsu.edu>
2017-09-18 16:46:47 -0400

    Ad buy/sell signals to crypto runtime

--- crypto/runtime.py
+++ crypto/runtime.py
@@ -4,3 +4,22 @@ class Runtime(object):
     ''' Stores argument information '''
     def __init__(self, args):
         self.stamp = args['time']
+        self.revert_signal()
+
+
+    def revert_signal(self):
+        ''' Resets buy/sell signal '''
+        self.signal = ''
+        self.confidence = 0
+
+
+    def signal_buy(self, confidence):
+        ''' Signals a buy, with an integer confidence between 0 and 100 '''
+        self.signal = 'BUY'
+        self.confidence = confidence
+
+
+    def signal_sell(self, confidence):
+        ''' Signals a sell, with an integer confidence between 0 and 100 '''
+        self.signal = 'SELL'
+        self.confidence = confidence
