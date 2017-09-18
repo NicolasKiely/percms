99449906e16920dff65c990e9eb971c9298f5caf
nmkiely <nmkiely@ncsu.edu>
2017-09-18 14:29:42 -0400

    Sort candlestick data in admin page

--- crypto/admin.py
+++ crypto/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
 
+class Candle_Stick_Admin(admin.ModelAdmin):
+    ordering = ('-stamp',)
+
 # Register your models here.
 admin.site.register(models.API_Key)
 admin.site.register(models.Currency)
+admin.site.register(models.Pair)
+admin.site.register(models.Candle_Stick, Candle_Stick_Admin)
