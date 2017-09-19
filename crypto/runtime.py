00d1b1e459bda8a97447bdd863569745606eff04
nmkiely <nmkiely@ncsu.edu>
2017-09-18 23:05:37 -0400

    Separate Runtime from settings in crypto

--- crypto/runtime.py
+++ crypto/runtime.py
@@ -1,13 +1,39 @@
 ''' Runtime library for crypto trading '''
 from . import models
 
+BUY_SIGNAL = 'BUY'
+SELL_SIGNAL = 'SELL'
+
 
 class Runtime(object):
     ''' Stores argument information '''
+    def __init__(self, data):
+        self.signal = ''
+        self.confidence = 0
+        self.data = data
+
+    def set_time(self, stamp):
+        ''' Sets current time '''
+        self.stamp = stamp
+
+
+    def signal_buy(self, confidence=100):
+        ''' Signals a buy, with an integer confidence between 0 and 100 '''
+        self.signal = BUY_SIGNAL
+        self.confidence = confidence
+
+
+    def signal_sell(self, confidence=100):
+        ''' Signals a sell, with an integer confidence between 0 and 100 '''
+        self.signal = SELL_SIGNAL
+        self.confidence = confidence
+
+
+class Runtime_Factory(object):
     def __init__(self, currencies):
-        self.revert_signal()
         self.currencies = currencies
         self.candles = None
+        self.num_candles = 0
 
 
     def load_data(self, t_start=None, t_end=None, period=300):
@@ -17,22 +43,9 @@ class Runtime(object):
         if t_end: filter_args['stamp__lt']=t_end
         self.candles = self.currencies.candle_stick_set.filter(
             **filter_args
-        ).order_by('stamp').all()
+        ).order_by('stamp').all()[:]
+        self.num_candles = len(self.candles)
 
 
-    def revert_signal(self):
-        ''' Resets buy/sell signal '''
-        self.signal = ''
-        self.confidence = 0
-
-
-    def signal_buy(self, confidence):
-        ''' Signals a buy, with an integer confidence between 0 and 100 '''
-        self.signal = 'BUY'
-        self.confidence = confidence
-
-
-    def signal_sell(self, confidence):
-        ''' Signals a sell, with an integer confidence between 0 and 100 '''
-        self.signal = 'SELL'
-        self.confidence = confidence
+    def runtime(self, i):
+        return Runtime(self.candles[:i])
