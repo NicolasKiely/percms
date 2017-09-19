a29784c5ec03c32659eeaa9427399614baf4d188
nmkiely <nmkiely@ncsu.edu>
2017-09-18 20:12:08 -0400

    Separe crypto runtime's data loading into own function

--- crypto/runtime.py
+++ crypto/runtime.py
@@ -1,10 +1,23 @@
 ''' Runtime library for crypto trading '''
+from . import models
+
 
 class Runtime(object):
     ''' Stores argument information '''
-    def __init__(self, args):
-        self.stamp = args['time']
+    def __init__(self, currencies):
         self.revert_signal()
+        self.currencies = currencies
+        self.candles = None
+
+
+    def load_data(self, t_start=None, t_end=None, period=300):
+        ''' Loads data between time periods '''
+        filter_args = {'period': period}
+        if t_start: filter_args['stamp__gte']=t_start
+        if t_end: filter_args['stamp__lt']=t_end
+        self.candles = self.currencies.candle_stick_set.filter(
+            **filter_args
+        ).order_by('stamp').all()
 
 
     def revert_signal(self):
