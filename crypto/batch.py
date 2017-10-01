229309514753383c8def670ec910d6851b7ce9bf
nmkiely <nmkiely@ncsu.edu>
2017-10-01 17:34:33 -0400

    Add poloniex scraper

--- crypto/batch.py
+++ crypto/batch.py
@@ -7,6 +7,7 @@ from scripting.utils import get_script_by_name
 from . import models
 from . import utils
 from . import runtime as crypto_runtime
+from . import poloniex_api
 import filemanager.utils
 
 
@@ -14,6 +15,34 @@ import filemanager.utils
 # Exception to log on backtest results page
 class Backtest_Exception(Exception):
     pass
+
+
+def pull_candle_data(polo, c1, c2, t_start, t_end):
+    ''' Pulls candlestick data for given currency pair in time frame '''
+    pair_name = c1+'_'+c2
+    exc, _ = crypto.models.Exchange.objects.get_or_create(name='Poloniex')
+    pair, _ = crypto.models.Pair.objects.get_or_create(c1=c1, c2=c2, exc=exc)
+    period = 300
+    data = polo.returnChartData(pair_name, start=t_start, end=t_end, period=period)
+    for x in data:
+        dt = datetime.datetime.fromtimestamp(x['date'])
+        dtz = timezone.make_aware(dt, timezone.get_current_timezone())
+        try:
+            candle = crypto.models.Candle_Stick.objects.get(
+                pair = pair, stamp = dtz
+            )
+        except crypto.models.Candle_Stick.DoesNotExist:
+            candle = crypto.models.Candle_Stick(pair=pair, stamp=dtz)
+
+        candle.p_high    = x['high']
+        candle.p_low     = x['low']
+        candle.p_close   = x['close']
+        candle.p_open    = x['open']
+        candle.volume    = x['volume']
+        candle.q_volume  = x['quoteVolume']
+        candle.w_average = x['weightedAverage']
+        candle.period    = period
+        candle.save()
 
 
 def run_backtest(backtest, fout):
@@ -45,13 +74,14 @@ def run_backtest(backtest, fout):
         if runtime.confidence > 90:
             # Calculate transfer from cur1 to cur2
             if runtime.signal == crypto_runtime.BUY_SIGNAL:
-                #print 'Buy!'
+                # Buy
                 buy_signal = 1
                 transfer = currency_1_amount
                 currency_1_amount -= transfer
                 currency_2_amount += transfer_percentage * (transfer / candle.p_close)
+
             elif runtime.signal == crypto_runtime.SELL_SIGNAL:
-                #print 'Sell!'
+                # Sell
                 buy_signal = -1
                 transfer = currency_2_amount
                 currency_1_amount += transfer_percentage * (transfer * candle.p_close)
@@ -110,3 +140,18 @@ def POST_backtest(currencies, exchange_name, script_name, dt_start, dt_stop):
     # Save results
     backtest.finished = True
     backtest.save()
+
+
+def POST_poloniex_candles_pull(currencies, dt_start, dt_stop, api_key_name):
+    ''' Handler for pulling down candlestick data for poloniex '''
+    # Get api key
+    try:
+        api_key = models.API_Key.objects.get(name=api_key_name)
+    except models.API_Key.DoesNotExist:
+        print 'API key "%s" not found' % api_key_name
+        return
+
+    polo = crypto.poloniex_api.poloniex(api_key.key, api_key.secret)
+
+    t_start = int(time.mktime(dt_start.timetuple()))
+    t_end = t_start + day_count*(60*60*24)
