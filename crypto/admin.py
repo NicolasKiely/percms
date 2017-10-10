a7736220b4f4715212bbd9206cfb962b723ba0c1
nmkiely <nmkiely@ncsu.edu>
2017-10-10 13:12:47 -0400

    Add exchange to django admin panel

--- crypto/admin.py
+++ crypto/admin.py
@@ -6,6 +6,7 @@ class Candle_Stick_Admin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(models.API_Key)
+admin.site.register(models.Exchange)
 admin.site.register(models.Currency)
 admin.site.register(models.Pair)
 admin.site.register(models.Candle_Stick, Candle_Stick_Admin)
